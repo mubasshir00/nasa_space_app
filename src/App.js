32ab8aeb4ee2a40404acaa3ecde1cb65817d8ad8
Mubasshir Ahmed <30567773+mubasshir00@users.noreply.github.com>
2023-10-06 12:25:59 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,128 @@
+/** @format */
+
+import "./App.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import Swiperimage from "./images_com/Swiperimage";
+// import "swiper/css";
+import "./App.css";
+import { useEffect } from "react";
+import Hero from "./components/Hero";
+import Explore from "./explore/Explore";
+import { Navigation } from "./components/Navigation";
+import Teammember from "./components/Teammember";
+import ParallaxHero from "./components/ParallaxHero";
+import About from "./components/About";
+import Herocontentmoon from "./components/Herocontentmoon";
+import Howtouse from "./components/Howtouse";
+
+function App() {
+	// useEffect(() => {
+	//   const sections = [...document.querySelectorAll('section')]
+
+	//   let options = {
+	//     rootMargin: '0px',
+	//     threshold: 0.75,
+	//   }
+
+	//   const callback = (entries, observer) => {
+	//     entries.forEach(entry => {
+	//       const { target } = entry
+
+	//       if (entry.intersectionRatio >= 0.75) {
+	//         target.classList.add('is-visible')
+	//       } else {
+	//         target.classList.remove('is-visible')
+	//       }
+	//     })
+	//   }
+
+	//   const observer = new IntersectionObserver(callback, options)
+
+	//   sections.forEach((section, index) => {
+	//     const sectionChildren = [
+	//       ...section.querySelector('[data-content]').children,
+	//     ]
+
+	//     sectionChildren.forEach((el, index) => {
+	//       el.style.setProperty('--delay', `${index * 250}ms`)
+	//     })
+
+	//     observer.observe(section)
+	//   })
+	// }, [])
+
+	return (
+		<div>
+			<Navigation />
+			<main
+				className='space-y-[50px] bg-gray-100 pb-12'
+				// style={{
+				//   background: 'url("background.jpg")',
+				// }}
+			>
+				{/* <Hero /> */}
+				<ParallaxHero />
+				{/* <Herocontentmoon/> */}
+
+				<section id='gallery' className='section container mx-auto px-4'>
+					<div class='section__content text-center' data-content>
+						<h1
+							className='text-[5rem] font-bold text-black mb-12'
+							style={{
+								fontFamily: "Raleway",
+							}}>
+							Moonquake Locations
+						</h1>
+
+						<Swiperimage />
+					</div>
+				</section>
+
+				<section id='threedview' class='section container mx-auto px-4'>
+					<div
+						class='section__content flex flex-col items-center justify-center gap-[50px]'
+						data-content>
+						<h1
+							className='text-[5rem] font-bold text-black'
+							style={{
+								fontFamily: "Raleway",
+							}}>
+							Visualize in 3D
+						</h1>
+
+						<Explore />
+					</div>
+				</section>
+
+				<section
+					id='howtouse'
+					class='section container mx-auto px-4 flex flex-col gap-[50px]'>
+					<h1
+						className='text-[5rem] font-bold text-black text-center'
+						style={{
+							fontFamily: "Raleway",
+						}}>
+						For offline usage
+					</h1>
+					<div class='section__content' data-content>
+						<Howtouse />
+					</div>
+				</section>
+
+				<section id='about' class='section container mx-auto px-4'>
+					<div class='section__content' data-content>
+						<About />
+					</div>
+				</section>
+
+				<section id='teammember' class='section container mx-auto px-4'>
+					<div class='section__content' data-content>
+						<Teammember />
+					</div>
+				</section>
+			</main>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/Teammember.js
@@ -0,0 +1,138 @@
+import React from 'react'
+import './Teammember.css'
+const Teammember = () => {
+  function ImageWrapper({ src, style }) {
+    return (
+      <div
+        style={{
+          width: '100%',
+        }}
+      >
+        <div
+          style={{
+            overflow: 'hidden',
+            position: 'relative',
+            width: '100%',
+            paddingBottom: '95%',
+          }}
+        >
+          <div
+            style={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '100%',
+            }}
+          >
+            <img
+              src={src}
+              alt=""
+              style={{
+                objectFit: 'cover',
+                height: '100%',
+                width: '100%',
+                ...style,
+              }}
+            />
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  return (
+		<div className='team-container'>
+			<div className='grid'>
+				<div className='card'>
+					<div className='card_img'>
+						<ImageWrapper src={"/moonquake/kazi.jpg"} />
+					</div>
+					<div className='card_body'>
+						<h2 className='card_title'>kazi Hasib</h2>
+						<h6 className='designation'>Team Lead</h6>
+						{/* <div className="call_btn">
+              <a href="#" className="btn_icon">
+                <i className="fas fa-phone-alt" />
+                <span className="circle" />
+              </a>
+            </div> */}
+					</div>
+				</div>
+				<div className='card'>
+					<div className='card_img'>
+						<ImageWrapper src={"/moonquake/syed.png"} />
+					</div>
+					<div className='card_body'>
+						<h2 className='card_title'>Syed Rubayyat Akbar</h2>
+						<h6 className='designation'>VR Developer</h6>
+						{/* <div className="call_btn">
+              <a href="#" className="btn_icon">
+                <i className="fas fa-phone-alt" />
+                <span className="circle" />
+              </a>
+            </div> */}
+					</div>
+				</div>
+				<div className='card'>
+					<div className='card_img'>
+						<ImageWrapper
+							src={"/moonquake/mim.jpeg"}
+							style={{
+								objectPosition: "0 10%",
+							}}
+						/>
+					</div>
+					<div className='card_body'>
+						<h2 className='card_title'>Jannatul Ferdous Mim</h2>
+						<h6 className='designation'>Business Analyst</h6>
+					</div>
+				</div>
+			</div>
+			<div className='grid'>
+				<div className='card'>
+					<div className='card_img'>
+						<ImageWrapper src={"/moonquake/Mubasshir.jpg"} />
+					</div>
+					<div className='card_body'>
+						<h2 className='card_title'>Mubasshir Ahmed</h2>
+						<h6 className='designation'>Software Engineer</h6>
+						{/* <div className="call_btn">
+              <a href="#" className="btn_icon">
+                <i className="fas fa-phone-alt" />
+                <span className="circle" />
+              </a>
+            </div> */}
+					</div>
+				</div>
+				{/* <div className="card">
+          <div className="card_img">
+            <ImageWrapper src={'/moonquake/wasiul.jpg'} />
+          </div>
+          <div className="card_body">
+            <h2 className="card_title">Wasiul Islam</h2>
+            <h6 className="designation">Frontend Developer</h6>
+            
+          </div>
+        </div> */}
+				<div className='card'>
+					<div className='card_img'>
+						<ImageWrapper src={"/moonquake/shanto_n.png"} />
+					</div>
+					<div className='card_body'>
+						<h2 className='card_title'>Sajid Hasan Shanta</h2>
+						<h6 className='designation'>UI/UX Designer</h6>
+						{/* <div className="call_btn">
+              <a href="#" className="btn_icon">
+                <i className="fas fa-phone-alt" />
+                <span className="circle" />
+              </a>
+            </div> */}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Teammember
